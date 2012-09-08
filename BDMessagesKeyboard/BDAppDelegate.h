cdc36182951f81457096e0e53a11980d20923fe1
nor <nor>
2012-09-08 11:29:55 +0700

    Initial Commit

---
+++ BDMessagesKeyboard/BDAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BDAppDelegate.h
+//  BDMessagesKeyboard
+//
+//  Created by norsez on 9/8/12.
+//  Copyright (c) 2012 Bluedot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BDViewController;
+
+@interface BDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) BDViewController *viewController;
+
+@end
---
+++ BDMessagesKeyboard/BDViewController.m
@@ -0,0 +1,34 @@
+//
+//  BDViewController.m
+//  BDMessagesKeyboard
+//
+//  Created by norsez on 9/8/12.
+//  Copyright (c) 2012 Bluedot. All rights reserved.
+//
+
+#import "BDViewController.h"
+
+@interface BDViewController ()
+
+@end
+
+@implementation BDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
