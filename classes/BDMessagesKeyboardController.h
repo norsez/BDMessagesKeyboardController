edc7a2c6231d4afd91440bc815332308b0c2e44d
norsez <norsez@gitcommit>
2012-09-13 15:47:31 +0700

    add code comments

--- classes/BDMessagesKeyboardController.h
+++ classes/BDMessagesKeyboardController.h
@@ -43,28 +43,92 @@
 
 }
 
+/**
+ Display text editor on the view of a controller.
+ @param viewControllerToDisplayOn view controller whose view will contain the text editor.
+ */
 - (void)showOnViewController:(UIViewController*)viewControllerToDisplayOn;
+
+/**
+ Display text editor on the view of a controller and adjust the given scrollview so that
+ the given subview on the scrollview will appear above the text editor.
+ @param viewControllerToDisplayOn view controller whose view will contain the text editor.
+ @param scrollView the scrollView which will be adjusted.
+ @param subViewInScrollView a subview on scrollView
+  */
 - (void)showOnViewController:(UIViewController *)viewControllerToDisplayOn adjustingScrollView:(UIScrollView*)scrollView forScrollViewSubview:(UIView*)subViewInScrollView;
+
+/**
+ Hide the displayed text editor.
+ */
 - (void)hide;
+
+/**
+ Clear the text editor's text.
+ */
 - (void)clearTextView;
 
 
 /**
  @name Customize Look and Feel.
  */
+
+/**
+ Initial height of the text editor.
+ */
 @property (nonatomic, assign) CGFloat editorHeight;
+
+/**
+ Flag to show a dedicated cancel button.
+ */
 @property (nonatomic, assign) BOOL showCancelButton;
+
+/**
+ Block called every time the text editor will be displayed. 
+ This block is given with the background view of the editor.
+ Customization can be done here.
+ */
 @property (nonatomic, copy) void(^styleBackgroundView)(UIView*);
+
+/**
+ Block called every time the text editor will be displayed.
+ This block is given with the text view of the editor.
+ Customization can be done here.
+ */
 @property (nonatomic, copy) void(^styleTextView)(UITextView*);
+
+/**
+ Block called every time the text editor will be displayed.
+ This block is given with the cancel button of the editor.
+ Customization can be done here.
+ */
 @property (nonatomic, copy) void(^styleCancelButton)(UIButton*);
+
+/**
+ Block called every time the text editor will be displayed.
+ This block is given with the done button of the editor.
+ Customization can be done here.
+ */
 @property (nonatomic, copy) void(^styleDoneButton)(UIButton*);
+
+/**
+ The view which is displayed on top of the text editor. 
+ This view should not respond to user interactions and it should be
+ automatically resized which the size of the text editor changes.
+ */
 @property (nonatomic, strong) UIView* accessoryView;
 
 /**
  @name Event Handling.
  */
-
+/**
+ Block called when the cancel button is pressed.
+ */
 @property (nonatomic, copy) void(^didPressCancel)(NSString*);
+
+/**
+ Block called just before the text editor is displayed.
+ */
 @property (nonatomic, copy) void(^willShowKeyboard)(UITextView*);
 
 /**
